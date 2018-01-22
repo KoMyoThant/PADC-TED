8c9a9f20022f7b1b9920c46a707c2cc6a1f7a6af
Myo Thant <myothantktt@gmail.com>
2018-01-22 23:34:46 +0630

    Project structure skeleton generation

---
+++ app/src/main/java/com/ps/ted/TEDApp.java
@@ -0,0 +1,17 @@
+package com.ps.ted;
+
+import android.app.Application;
+
+/**
+ * Created by pyaesone on 1/22/18.
+ */
+
+public class TEDApp extends Application {
+
+    public static final String LOG_TAG = "TEDApp";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
